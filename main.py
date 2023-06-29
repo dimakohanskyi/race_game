4001741e3806550993db282240671e2de00e22fc
dimakohanskyi <dimakohanskyi@gmail.com>
2023-06-29 15:55:46 +0300

    Add main features of project

---
+++ main.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=800, height=600)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "green", "blue", "purple", "orange", "yellow"]
+y_position = [-180, -90, 0, 90, 180]
+all_turtles = []
+
+
+for turtle_index in range(0, 5):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-380, y=y_position[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 380:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! {winning_color} turtle is the winner!")
+
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+
+
+screen.exitonclick()
